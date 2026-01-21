8f500f50209eb7c4f41689f1dc99fdb7396e91ec
TofuMango <yjlee0006@cu.ac.kr>
2026-01-21 21:28:14 +0900

    2.3 Tailwind CSS 핵심정리2

--- miniSNS/src/App.tsx
+++ miniSNS/src/App.tsx
@@ -24,9 +24,23 @@ function App() {
       {/* 3. 사이즈 */}
       <div className="h-20 w-full bg-blue-500">box</div>
 
-      {/* 4. 여백 - m: 바깥여백, p: 내부여백*/}
+      {/* 4. 여백 - m: 바깥여백, p: 내부여백 */}
       <div className="m-5 h-50 w-50 bg-red-400 px-5 py-5">
         <div className="h-full w-full bg-blue-400"></div>
+      </div>
+
+      {/* 5. 보더: 요소의 경계선 */}
+      <div className="m-5 rounded-md border-x-2 border-y-2 border-x-blue-500">
+        border
+      </div>
+
+      {/* 6. 플렉스 컨테이너 */}
+      <div className="flex flex-row items-start justify-evenly">
+        <div className="h-10 w-10 border">1</div>
+        {/* flex-1 최대한 넓게(남는자리중에서) */}
+        <div className="h-20 w-10 flex-1 border">1</div>
+        <div className="h-30 w-10 border">1</div>
+        <div className="h-40 w-10 border">1</div>
       </div>
     </div>
   );
