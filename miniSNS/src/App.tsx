586c6ec85bb454fc0364cba33b09ee6f9b1349b3
TofuMango <yjlee0006@cu.ac.kr>
2026-01-26 22:03:35 +0900

    2.5 Shadcn/ui 핵심정리1

--- miniSNS/src/App.tsx
+++ miniSNS/src/App.tsx
@@ -1,12 +1,35 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Textarea } from "./components/ui/textarea";
+import { Toaster } from "./components/ui/sonner";
+import { toast } from "sonner";
 import { cn } from "./lib/utils";
 
 function App() {
   const isActive = true;
   return (
-    <div>
-      <Button>버튼</Button>
+    <div className="p-5">
+      <Toaster />
+      <Textarea />
+
+      <Input placeholder="입력..." />
+
+      <Button
+        onClick={() => {
+          toast("토스트메시지", {
+            position: "top-center",
+          });
+        }}
+      >
+        버튼!
+      </Button>
+      <Button variant={"destructive"}>버튼</Button>
+      <Button variant={"ghost"}>버튼</Button>
+      <Button variant={"link"}>버튼</Button>
+      <Button variant={"outline"}>버튼</Button>
+      <Button variant={"secondary"}>버튼</Button>
+
       <div
         className={cn(
           "w-10 text-xl",
