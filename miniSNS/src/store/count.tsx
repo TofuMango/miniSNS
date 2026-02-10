c665aff6f684ffc44c53a25750a3ff2b02d29914
TofuMango <yjlee0006@cu.ac.kr>
2026-02-10 17:24:42 +0900

    3.4 Zustand 미들웨어1

--- miniSNS/src/store/count.tsx
+++ miniSNS/src/store/count.tsx
@@ -1,34 +1,58 @@
 import { create } from "zustand";
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
+// combine / immer 미들웨어 사용
+export const useCountStore = create(
+  immer(
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        // action 함수
+        increase: () => {
+          set((state) => {
+            state.count += 1;
+          });
+        },
 
-// 제네릭 타입변수로 Store를 제공
-export const useCountStore = create<Store>((set, get) => ({
-  // state: count
-  count: 0,
-  actions: {
-    // action 함수
-    increase: () => {
-      // const count = get().count;
-      // set({ count: count + 1 });
-      set((store) => ({
-        count: store.count + 1,
-      }));
-    },
+        decrease: () => {
+          set((state) => {
+            state.count -= 1;
+          });
+        },
+      },
+    })),
+  ),
+);
 
-    decrease: () => {
-      set((store) => ({
-        count: store.count - 1,
-      }));
-    },
-  },
-}));
+// type Store = {
+//   count: number;
+//   actions: {
+//     increase: () => void;
+//     decrease: () => void;
+//   };
+// };
+
+// // 제네릭 타입변수로 Store를 제공
+// export const useCountStore = create<Store>((set, get) => ({
+//   // state: count
+//   count: 0,
+//   actions: {
+//     // action 함수
+//     increase: () => {
+//       // const count = get().count;
+//       // set({ count: count + 1 });
+//       set((store) => ({
+//         count: store.count + 1,
+//       }));
+//     },
+
+//     decrease: () => {
+//       set((store) => ({
+//         count: store.count - 1,
+//       }));
+//     },
+//   },
+// }));
 
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
