0798c8e855ceabd7271bc4b83abee8302a63405e
TofuMango <yjlee0006@cu.ac.kr>
2026-03-03 18:45:29 +0900

    3.7 투두리스트 기능 구현

--- miniSNS/src/components/todo-list/todo-editor.tsx
+++ miniSNS/src/components/todo-list/todo-editor.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { useCreateTodo } from "@/store/todos";
 
 export default function TodoEditor() {
+  const createTodo = useCreateTodo();
+  const [content, setContent] = useState("");
+
+  const handleAddClick = () => {
+    // content가 공백일 시 return
+    if (content.trim() === "") return;
+    createTodo(content);
+    // 입력값 초기화
+    setContent("");
+  };
   return (
     <div className="flex gap-2">
-      <Input placeholder="새로운 할 일 입력" />
-      <Button>추가</Button>
+      <Input
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="새로운 할 일 입력"
+      />
+      <Button onClick={handleAddClick}>추가</Button>
     </div>
   );
 }
---
+++ miniSNS/src/store/todos.tsx
@@ -0,0 +1,45 @@
+import { create } from "zustand";
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+import type { Todo } from "@/types";
+
+const initialState: {
+  todos: Todo[];
+} = {
+  todos: [],
+};
+const useTodosStore = create(
+  immer(
+    combine(initialState, (set) => ({
+      actions: {
+        createTodo: (content: string) => {
+          set((state) => {
+            state.todos.push({ id: new Date().getTime(), content: content });
+          });
+        },
+        deleteTodo: (targetId: number) => {
+          set((state) => {
+            state.todos = state.todos.filter((todo) => todo.id != targetId);
+          });
+        },
+      },
+    })),
+  ),
+);
+
+// 커스텀 hook
+// todos가 바뀔때만 리렌더 시킴
+export const useTodos = () => {
+  const todos = useTodosStore((store) => store.todos);
+  return todos;
+};
+
+export const useCreateTodo = () => {
+  const createTodo = useTodosStore((store) => store.actions.createTodo);
+  return createTodo;
+};
+
+export const useDeleteTodo = () => {
+  const deleteTodo = useTodosStore((store) => store.actions.deleteTodo);
+  return deleteTodo;
+};
