c04d6d9d68ac2fea49ca502798d77de997e757a4
TofuMango <yjlee0006@cu.ac.kr>
2026-03-18 17:34:31 +0900

    4.7 데이터 무효화

--- miniSNS/src/hooks/mutations/use-create-todo-mutation.ts
+++ miniSNS/src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,13 +1,17 @@
 import { createTodos } from "@/api/create-todos";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateTodoMutaiton() {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: createTodos,
     onMutate: () => {}, // 요청 발송 시
     onSettled: () => {}, // 요청 종료 시
     onSuccess: () => {
-      window.location.reload();
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.todo.list,
+      });
     }, // 요청 성공 시
     onError: (error) => {
       window.alert(error.message);
